f2bfd874964b41f42165758858bd54a40a318a5c
Matias Rojas <maigrojas.dev@gmail.com>
2021-08-17 21:55:42 -0400

    Re build y limpieza de componentes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './assets/styles/_settings.scss'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Projects from './components/Projects';
-import Contact from './components/Contact';
 import Nav from './components/Nav';
 import MainScreen from './components/MainScreen'
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 const App = () => (
   <BrowserRouter>
     <Nav />
     <Switch>
-      <Route exact path='/' component={MainScreen} />
+      <Route exact path='/home' component={MainScreen} />
+      <Redirect to="/home" />
     </Switch>
   </BrowserRouter>
 )
