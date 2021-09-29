acbc50f0c68a783452335dd673bad9a4ab925e1c
Matias Rojas <maigrojas.dev@gmail.com>
2021-09-29 18:32:54 -0300

    Remove the unused dependencies

--- config/webpack.prod.js
+++
@@ -1,9 +0,0 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common')
-
-/** @type {import('webpack').Configuration} */
-const prodConfig = {
-    mode: 'production',
-}
-
-module.exports = merge(common, prodConfig)
