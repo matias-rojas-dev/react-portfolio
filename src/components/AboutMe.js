d411372d7097be8c49f0eff5c2a4d57e002c2646
matias-rojas-dev <you@example.com>
2022-09-16 11:53:05 -0300

    Change the personal description

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,10 +7,8 @@ const AboutMe = () => (
       <h1>Matías Rojas Rojas</h1>
       <h2>Desarrollador Web</h2>
       <p>
-        Desarrollador con conocimientos tanto en tecnologías de Front-end 
-        como en Back-end Principalmente autodidacta con conocimientos en
-        tecnologías de JavaScript, React, NodeJS, MongoDB, Express,
-        MySQL, entre otros.
+        Desarrollador con conocimientos en tecnologías tanto de Front-end 
+        como de Back-end
       </p>
     </div>
   </AboutMeContainer>
